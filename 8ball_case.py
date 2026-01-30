f4c909b90eb4230c7aa09ea5cff8e854c7b4cfed
Gabriel <gabriel.perez910@gmail.com>
2026-01-30 17:45:01 -0600

    Update 8ball_case.py

--- 8ball_case.py
+++ 8ball_case.py
@@ -5,7 +5,7 @@ import random
 name = "Bob"
 # question = input("What is your question: ")
 question = "Are you my mommy?"
-8_ball_q = "Magic 8-ball's answer: "
+ball8_q = "Magic 8-ball's answer: "
 final_q = name + " asks " + question
 answer = ""
 random_number = random.randint(1, 9)
@@ -21,39 +21,39 @@ if name != "":
       case 1:  
         answer = "Yes - definitely"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 2:
         answer = "It is decidedly so"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 3:
         answer = "Without a doubt"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 4:
         answer = "Reply hazy, try again"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 5:
         answer = "Ask again later"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 6:
         answer = "Better not tell you now"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 7:
         answer = "My sources say no"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 8:
         answer = "Outlook not so good"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 9:
         answer = "Very doubtful"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
   else:
     print("Hey now, you didn't input a question.")
 
@@ -62,39 +62,39 @@ else:
       case 1:  
         answer = "Yes - definitely"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 2:
         answer = "It is decidedly so"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 3:
         answer = "Without a doubt"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 4:
         answer = "Reply hazy, try again"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 5:
         answer = "Ask again later"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 6:
         answer = "Better not tell you now"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 7:
         answer = "My sources say no"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 8:
         answer = "Outlook not so good"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
       case 9:
         answer = "Very doubtful"
         print(final_q)
-        print(8_ball_q + answer)
+        print(ball8_q + answer)
   else:
     print("Hey now, you didn't input anything, huh.")
 
